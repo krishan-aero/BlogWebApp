a994fffc6da945ff781795948abb4445d8a7fd09
Krishan <75919169+krishan-aero@users.noreply.github.com>
2024-02-25 02:54:10 +0900

    added edit button and route for editing

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import bodyParser from "body-parser";
 const app  = express();
 const port = 3000;
 var blogList = [];
+var blogListEdit = [];
 var blogCount = 0;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
@@ -21,6 +23,7 @@ app.get("/refresh", (req, res) => {
 })
 
 app.post("/submit", (req, res) => {
+    console.log("submit is hit")
     blogCount++;
     var blog = {id: blogCount, blogType: req.body.blogType, inputText : req.body.blog}; 
     blogList.push(blog);
@@ -31,6 +34,18 @@ app.post("/submit", (req, res) => {
 app.post("/delete", (req, res) => {
     blogList = blogList.filter(item => (item.id).toString() !== req.body.blogID);
     res.render("index.ejs", {blogList: blogList}); 
+})
+
+app.post("/edit", (req, res) => {
+    console.log("In edit", blogList);
+    blogListEdit = blogList.filter(item => (item.id).toString() == req.body.blogID)[0];
+    console.log(blogListEdit);
+    res.render("index.ejs", {blogListEdit}); 
+})
+
+app.post("/resubmit", (req, res) => {
+    console.log(" resubmit", blogList);
+    res.redirect("/")
 })
 
 app.listen(port, () => {
