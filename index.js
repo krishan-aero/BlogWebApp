d46bfd01b57559a5bea5a1cdfc5cf5b8c8c8a4ff
Krishan <75919169+krishan-aero@users.noreply.github.com>
2024-02-25 15:22:18 +0900

    Added feature to edit and resubmit the blog

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ var blogCount = 0;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Home page
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+// got to home screen from any route
 app.get("/refresh", (req, res) => {
     if (blogCount > 0){
         blogCount = 0;
@@ -22,20 +24,23 @@ app.get("/refresh", (req, res) => {
     res.redirect("/");
 })
 
+// Submit the blog
 app.post("/submit", (req, res) => {
     console.log("submit is hit")
     blogCount++;
     var blog = {id: blogCount, blogType: req.body.blogType, inputText : req.body.blog}; 
-    blogList.push(blog);
+    blogList.unshift(blog);
     console.log(blogList);
     res.render("index.ejs", {blogList : blogList});
 })
 
+// delete the blog
 app.post("/delete", (req, res) => {
     blogList = blogList.filter(item => (item.id).toString() !== req.body.blogID);
     res.render("index.ejs", {blogList: blogList}); 
 })
 
+// Create a space to edit blog
 app.post("/edit", (req, res) => {
     console.log("In edit", blogList);
     blogListEdit = blogList.filter(item => (item.id).toString() == req.body.blogID)[0];
@@ -43,9 +48,13 @@ app.post("/edit", (req, res) => {
     res.render("index.ejs", {blogListEdit}); 
 })
 
+// ReSubmit the editted blog.
 app.post("/resubmit", (req, res) => {
-    console.log(" resubmit", blogList);
-    res.redirect("/")
+    console.log(" resubmit", req.body);
+    var blogEdited = [{id: req.body.blogID, blogType: req.body.blogType, inputText : req.body.blog}];
+    const i = blogList.findIndex(x => x.id == blogEdited.id)
+    blogList = blogList.map(x => blogEdited.find(({ id }) => id == x.id) || x)
+    res.render("index.ejs", {blogList : blogList});
 })
 
 app.listen(port, () => {
